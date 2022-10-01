af4b2ba524ad2fa83dbe323872812a368895f63c
AlphaJR Dev <alphajrdev@gmail.com>
2022-10-01 21:28:58 +0100

    AlphaJR

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center">
+  <br>
+  AlphaJR.
+  <br>
+</h1>
+
+<h3 align=center>AlphaJR36/portfolio_python001</h3>
+
+## 🍭 | About
+
+PT - Esta aplicação foi feita como um exemplo e não como a versão final.<br>
+EN - This application was made as an example and not as the final version.
+
+## 💻 | Technologies
+
+*    **Python**
+
+## 🎈 | Which Is?
+
+PT - É um Website responsivo. (Formulário)<br>
+EN - It's an application. (Form)
+
+## 📷 | Photos
+
+![AlphaJR.](https://i.imgur.com/5tFBBVR.png)
---
+++ form.py
@@ -0,0 +1,7 @@
+nome_question = input("\n\n\n\n\n\n\n\n\n\n\n\n\n\nOlá, qual é o seu nome?  ")
+idade_question = input("\n\nNossa " + nome_question + " que nome lindo! Qual sua idade?  ")
+irmaos_question_s_n = input("\n\nUau " + idade_question + " anos?? Ainda é bem novo... Você tem irmãos?  ")
+irmaos_question_quantidade = input("\n\nCerto, você tem irmãos. Quantos?  ")
+input_finally = input("\n\nPresione ENTER para avançar...")
+
+print("\n\n\n================\n" + "Dados de " + nome_question + "\n================\n\n" + "Nome: " + nome_question + "\nIdade: " + idade_question + "\nIrmãos: " + irmaos_question_s_n + "\nNúmero de irmãos: " + irmaos_question_quantidade )
